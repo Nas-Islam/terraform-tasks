161d25161481d64ebc8cd8c583b4742efc431899
Ubuntu <nas@terraformvm.biy4qgnnwmpe3jvpbe2sykybub.zx.internal.cloudapp.net>
2021-02-09 15:52:43 +0000

    Adding task3 files

--- task-3-environments/vmss/output.tf
+++ task-3-environments/vmss/output.tf
@@ -1,3 +1,11 @@
 output "vmss_public_ip" {
   value = azurerm_public_ip.vmss.fqdn
 }
+
+output "fdqn" {
+  value = random_string.fqdn.result
+}
+
+output "vmss_subnet_id" {
+  value = azurerm_subnet.vmss.id
+}
